ed7f68e40c919cff592cdce81f97b2302b61c0cc
grace <gracedes@bu.edu>
2025-12-27 18:09:52 -0500

    day02pt1

---
+++ day02/pt1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut acc: u64 = 0;
+    
+    let input: String = fs::read_to_string("../input.txt")?;
+    let ranges: Vec<_> = input.split(',').collect();
+
+    for range in ranges {
+        let bounds: Vec<_> = range.split('-').collect();
+        let lower_bound: u64 = bounds[0].parse().unwrap();
+        let upper_bound: u64 = bounds[1].trim().parse().unwrap();
+        
+        for i in lower_bound..(upper_bound + 1) {
+            if invalid(i) { acc += i }
+        }
+    }
+
+    println!("the sum of invalid IDs is: {acc}");
+    Ok(())
+}
+
+fn invalid(x: u64) -> bool {
+    let pow = (x.ilog10() + 1) / 2;
+    x / 10u64.pow(pow) == x % 10u64.pow(pow)
+}
